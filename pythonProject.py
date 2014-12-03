e19334a5f87b3f494befb93c8dae478e4bad28d0
BradenRupp <you@example.com>
2014-12-02 22:45:25 -0600

    added changes

--- pythonProject.py
+++ pythonProject.py
@@ -15,4 +15,25 @@ filename = "results.csv"
 
 df = pd.read_csv(filename, encoding='latin1')
 
+#print(df.iloc[1])
 
+#print(df.iloc[44])
+
+#print(df.iloc[44][3])
+
+test = str(df.iloc[44][0])
+
+if(test == 'nan'):
+    print('null')
+
+
+for x in df:
+    print(df.iloc[x])
+    
+    
+"""y = 0
+    while(y<8):
+        if(df[x][y] == null):"""
+            
+        
+    
