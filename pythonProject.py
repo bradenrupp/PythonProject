f1d69d5b076389fb57391b68670bcf832a9b6550
BradenRupp <you@example.com>
2014-12-05 16:48:05 -0600

    changes

--- pythonProject.py
+++ pythonProject.py
@@ -20,9 +20,6 @@ df = pd.read_csv(filename, encoding='latin1')
 #Drops any rows that do not contain any information in them
 df2 = df.dropna(how='any')
 
-mean = df.mean()
-
-
 #Strips off information we don't need about the date
 df['SaleDate'] = df['SaleDate'].map(lambda x: str(x)[6:])
 
@@ -32,4 +29,3 @@ df['SaleDate'] = df['SaleDate'].astype(int)
 #Display Averages of the Data for insight
 print ('The averages of the data: \n', np.round(df.mean(), decimals=2))
 
-df.plot.box
